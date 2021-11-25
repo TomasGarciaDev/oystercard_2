1aaffb26409b183d7ba855d4af5a1e8073dce112
Sarah <dousse.sarah@gmail.com>
2021-11-25 14:59:35 +0000

    TODO: refactor the doubles

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,42 +2,33 @@ require 'journey'
 require 'oystercard'
 
 describe Journey do
-  # context 'initialize'
-  # it 'created a object of itself' do
-  #   expect(subject).to be_an_instance_of(Journey)
-  # end 
-  
-  let(:station) { double(Station)}
-  let(:station2) { double(Station)}
+  before(:each) do
+    let(:card) { instance_double(Oystercard, top_up: 10) }
+    let(:Oval) { instance_double(Station, station_name: "oval", zone: 2) }
+    let(:Bow) { instance_double(Station, station_name: "bow", zone: 3) }
 
+    allow(:card).to receive(:touch_in).with_message(:oval)
+  end
   it 'has an entry station' do
-    # card = Oystercard.new
-    # card.top_up(20)
-    # card.touch_in(:station)
-    journey = Journey.new("Oval")
-    expect(journey.entry_station).to eq("Oval")
+    journey = Journey.new
+    :card.touch_in(:Oval)
+    expect(journey.entry_station).to eq(:Oval)
   end
 
-  it "has an exit station" do
+  xit "has an exit station" do
     journey = Journey.new
     expect(journey.exit_station).to eq nil
   end
 
-  # it 'should know when a journey is finish' do
-  #   expect(subject.finish).to be_an_instance_of(Hash)
-  # end
-
-  it 'should store the entry station at touch_in' do
+  xit 'should store the entry station at touch_in' do
     card =  Oystercard.new
     card.top_up(20)
     card.touch_in(:station)
     expect(card.journey.entry_station).to eq :station
   end
 
-  # it 'should return the in and out of a Journey in a hash' do
-  #   journey = Journey.new("Oval")
-  #   @exit_station = "Bow"
-  #   expect(journey.finish).to eq({:a=>"Oval", :b=>"Bow"})
+  # it 'should store the exit station at touch_out' do
+  #   card = Oystercard.new
   # end
 end
 
